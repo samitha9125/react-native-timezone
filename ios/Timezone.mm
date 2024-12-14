149d021a3bd8d944ac0bb3895466a29c2e48ec37
Samitha <samitha@Samitha-ML.local>
2024-12-14 14:27:20 +0530

    chore: updated the ios implementation

--- ios/Timezone.mm
+++ ios/Timezone.mm
@@ -1,18 +1,49 @@
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
 #import "Timezone.h"
 
 @implementation Timezone
-RCT_EXPORT_MODULE()
+    RCT_EXPORT_MODULE('Timezone');
+    /**
+    * This is a Synchronous method.
+    * Retrieves the current time zone name.
+    */
+    RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getTimeZone)
+    {
+        NSTimeZone *timeZone = [NSTimeZone localTimeZone];
+        return timeZone.name;
+    }
 
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
+    /**
+    * Synchronous method.
+    * Gets the region based on the telephony manager's network country ISO.
+    */
+    RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getRegionByTelephony)
+    {
+        CTTelephonyNetworkInfo *networkInfo = [[CTTelephonyNetworkInfo alloc] init];
+        CTCarrier *carrier = [networkInfo subscriberCellularProvider];
+        NSString *isoCountryCode = [carrier isoCountryCode];
+        if (isoCountryCode == nil || [isoCountryCode isEqualToString:@""]) {
+            return nil;
+        }
+        return isoCountryCode;
+    }
 
-    return result;
-}
+    /**
+    * Synchronous method.
+    * Retrieves the region based on the device's locale.
+    */
+    RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getRegionByLocale)
+    {
+        NSLocale *locale = [NSLocale currentLocale];
+        NSString *countryCode = [locale objectForKey:NSLocaleCountryCode];
+        return countryCode;
+    }
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeTimezoneSpecJSI>(params);
-}
+    - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+        (const facebook::react::ObjCTurboModule::InitParams &)params
+    {
+        return std::make_shared<facebook::react::NativeTimezoneSpecJSI>(params);
+    }
 
 @end
